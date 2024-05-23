b668bd190b3c0381f1e1d570643ae32907356db3
Riley DuBord <thelifeofdu@gmail.com>
2024-05-23 11:52:31 -0500

    devii tenant id

--- src/backend/Auth.js
+++ src/backend/Auth.js
@@ -8,9 +8,9 @@ const AUTH_URL = 'https://api.devii.io/auth';
 async function getAccessToken() {
     // Create an object to store the form data with your Devii credentials
     const data = {
-        login: 'username',
+        login: '',
         password: 'password',
-        tenantid: 10155,
+        tenantid: 10161,
     };
 
     try {
